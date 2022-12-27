b3cd2f34a8c921305ed9d55a2df069c5baad8e18
Your Name <you@example.com>
2022-12-27 03:11:36 -0300

    alpha v0.26

--- App/js/auth.js
+++ App/js/auth.js
@@ -5,3 +5,7 @@ var btnPass = document.getElementById('pass');
 btnLogin.addEventListener('click', function(){
     
 });
+
+function redirectTest(){
+    window.location.href = '../html/desktop.html';
+};
