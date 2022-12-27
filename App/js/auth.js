505367f9e6c9b21ccbec242a837b9f6a352a4a5e
Your Name <you@example.com>
2022-12-27 02:19:05 -0300

    interface alpha v000.25

---
+++ App/js/auth.js
@@ -0,0 +1,7 @@
+var btnLogin = document.getElementById('btn-login');
+var btnId = document.getElementById('id');
+var btnPass = document.getElementById('pass');
+
+btnLogin.addEventListener('click', function(){
+    
+});
