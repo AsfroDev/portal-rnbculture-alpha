af5fffdb2f0aa40444080a54091d2a06abe57850
Your Name <you@example.com>
2022-12-27 13:45:38 -0300

    alpha v0.27

--- App/js/auth.js
+++ App/js/auth.js
@@ -3,9 +3,12 @@ var btnId = document.getElementById('id');
 var btnPass = document.getElementById('pass');
 
 btnLogin.addEventListener('click', function(){
-    
+    event.preventDefault()
+    if (btnPass.value == 'api9632') {
+        console.log('pass');
+        window.location.href = '../html/desktop.html';
+    } else {
+        console.log('invalid');
+    }
 });
 
-function redirectTest(){
-    window.location.href = '../html/desktop.html';
-};
